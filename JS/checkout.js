0bb016775bfba6cb8b02be523dd9e53ae91d895a
Roh1514 <152458779+Roh1514@users.noreply.github.com>
2025-01-14 13:47:37 -0300

    Add files via upload

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
     const cartItemsContainer = document.getElementById('cart-items');
-    let totalQuantity = 0;
-    let totalPrice = 0;
+    let totalQuantidade = 0;
+    let totalPreço = 0;
 
     cart.forEach(item => {
         const li = document.createElement('li');
-        li.textContent = `${item.name} - Quantity: ${item.quantity} - Price: R$${(item.price * item.quantity).toFixed(2)}`;
+        li.textContent = `${item.name} - Quantidade: ${item.quantity} - Preço: R$${(item.price * item.quantity).toFixed(2)}`;
         cartItemsContainer.appendChild(li);
 
-        totalQuantity += item.quantity;
-        totalPrice += item.price * item.quantity;
+        totalQuantidade += item.quantity;
+        totalPreço += item.price * item.quantity;
     });
-    document.getElementById('total-quantity').textContent = totalQuantity;
-    document.getElementById('total-price').textContent = `R$${totalPrice.toFixed(2)}`;
+    document.getElementById('total-quantity').textContent = totalQuantidade;
+    document.getElementById('total-price').textContent = `R$${totalPreço.toFixed(2)}`;
 
     document.getElementById('confirm-checkout').addEventListener('click', function () {
         alert('Checkout processado com sucesso!');
